e853d7b99550e0194e4150ef88202c05248da94a
Bridger Maxwell <bridgeyman@gmail.com>
2014-08-01 09:15:03 -0400

    Initial Commit

---
+++ SwiftVisualFormat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftVisualFormat
+//
+//  Created by Bridger Maxwell on 8/1/14.
+//  Copyright (c) 2014 Bridger Maxwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SwiftVisualFormatTests/SwiftVisualFormatTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftVisualFormatTests.swift
+//  SwiftVisualFormatTests
+//
+//  Created by Bridger Maxwell on 8/1/14.
+//  Copyright (c) 2014 Bridger Maxwell. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SwiftVisualFormatTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
